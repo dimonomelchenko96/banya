2fe77bb05ade46824358a0274be518b78ac465ea
Дмитрій Омельченко <dimonomelchenko1@gmail.com>
2022-07-13 18:58:19 +0300

    edits

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // slider
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
-    loop: true,
+    // loop: true,
     spaceBetween: 20,
 
 
@@ -12,9 +12,6 @@ const swiper = new Swiper('.swiper-container', {
         clickable: true,
       },
       
-
-	
-
 	breakpoints: {
 		992: {
 			slidesPerView: 3,
@@ -47,26 +44,31 @@ const swiper = new Swiper('.swiper-container', {
 			if (parent.classList.contains('active')) {
 				parent.classList.remove('active');
 			} else{
-				accSection.forEach(child => {
-					child.classList.remove('active');
-					parent.classList.add('active');
-				});
+			accSection.forEach(child => {
+				child.classList.remove('active');
+				parent.classList.add('active');
+			});
 			}
 		});
 	});	 
 	// popap
 	const buttons = document.querySelectorAll('.button__goods'),
-	  popap = document.querySelector('.overlay'),
-	  popapClose = document.querySelector('.project-card__close');
+	  	  popap = document.querySelector('.overlay'),
+	      popapClose = document.querySelector('.project-card__close'),
+		  title = document.querySelector('.project-card__title');
 
 	buttons.forEach(btn => {
 		btn.addEventListener('click', () => {
+			let text = btn.closest('.goods__label').querySelector(".goods__label-title").textContent;
+			title.innerHTML = text;
 			popap.classList.add('active');
 		});
 	});
-	popapClose.addEventListener('click', () => {
-		popap.classList.remove('active');
-	});	
+	popap.addEventListener('click', (e) => {
+		if(e.target === popap || e.target === popapClose) {
+			popap.classList.remove('active');
+		}
+	});
 
 });
 
